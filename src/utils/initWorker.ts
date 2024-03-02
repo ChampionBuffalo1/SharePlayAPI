8b8eeedc487fb64f975f4ac6383e05d0de38d981
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-03-02 22:16:50 +0530

    feat: Added `getWorker` util

--- src/utils/initWorker.ts
+++ src/utils/initWorker.ts
@@ -1,42 +1,31 @@
 import os from 'node:os';
 import Logger from './Logger';
-import mediasoup from 'mediasoup';
-
-const mediaCodecs: mediasoup.types.RtpCodecCapability[] = [
-  {
-    kind: 'audio',
-    mimeType: 'audio/opus',
-    clockRate: 48000,
-    channels: 2,
-  },
-  {
-    kind: 'video',
-    mimeType: 'video/VP8',
-    clockRate: 90000,
-    parameters: { 'x-google-start-bitrate': 1000 },
-  },
-];
+import * as mediasoup from 'mediasoup';
+import MediaSoupConfig from '../rtcRelay/MediaSoupConfig';
 
 type IWorker = Array<{
   worker: mediasoup.types.Worker;
   router: mediasoup.types.Router;
 }>;
 
-export async function startMediaSoup(): Promise<IWorker> {
-  const workers: IWorker = [];
+export const workers: IWorker = [];
+
+export async function startMediaSoup(): Promise<void> {
   for (let i = 0; i < Object.keys(os.cpus()).length; i++) {
-    let worker = await mediasoup.createWorker({
-      rtcMinPort: 10000,
-      rtcMaxPort: 11000,
-    });
+    let worker = await mediasoup.createWorker(MediaSoupConfig.worker);
     worker.on('died', () => {
       Logger.error('Mediasoup worker died');
       process.exit(1);
     });
-    const router = await worker.createRouter({
-      mediaCodecs,
-    });
+    const router = await worker.createRouter(MediaSoupConfig.router);
     workers.push({ worker, router });
   }
-  return workers;
+  Logger.info('Mediasoup started');
+}
+
+let workerIdx = 0;
+export function getWorker() {
+  const worker = workers[workerIdx++];
+  workerIdx %= workers.length;
+  return worker;
 }
