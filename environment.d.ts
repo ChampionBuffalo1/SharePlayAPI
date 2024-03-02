b41adc9de0ba7feae49fe1c7cc9b88de3e02feca
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-03-02 21:59:37 +0530

    (feat): Added webrtc `env`

--- environment.d.ts
+++ environment.d.ts
@@ -2,6 +2,8 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       PORT: string;
+      LISTEN_IP?: string;
+      ANNOUNCED_IP?: string;
       NODE_ENV: 'production' | 'development';
     }
   }
